c4b0208ab107d761cd76870a3a98e303e0d2d6b7
bepicard <benjamin.picard@etu.uca.fr>
2019-04-17 12:54:47 +0100

    settings page (start and end of day)

--- src/screens/Podometer/WeekPodometer.js
+++ src/screens/Podometer/WeekPodometer.js
@@ -1,10 +1,11 @@
 import Expo from "expo";
 import React from "react";
 import { Pedometer } from "expo";
-import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import { StyleSheet, Text, View, Button, FlatList, ActivityIndicator } from "react-native";
 
 export default class DayPodometer extends React.Component {
   state = {
+    isLoading: true,
     isPedometerAvailable: "checking",
     pastStepCount: {
       monday: 0,
@@ -26,7 +27,7 @@ export default class DayPodometer extends React.Component {
   }
 
   _subscribe = () => {
-    Pedometer.isAvailableAsync().then(
+    /*Pedometer.isAvailableAsync().then(
       result => {
         this.setState({
           isPedometerAvailable: String(result)
@@ -37,14 +38,15 @@ export default class DayPodometer extends React.Component {
           isPedometerAvailable: "Could not get isPedometerAvailable: " + error
         });
       }
-    );
+    );*/
 
+    //const today = new Date("2019-04-14T23:00:00")
     const today = new Date()
     var nbDays = today.getDay();
     if(nbDays == 0) nbDays = 7
-    console.log(nbDays)
 
     for(i = 0; i < nbDays; i++) {
+      //var end = new Date("2019-04-14T23:00:00")
       var end = new Date()
       var start
       if((end.getMonth() + 1) >= 10) {
@@ -56,19 +58,25 @@ export default class DayPodometer extends React.Component {
       start.setDate(start.getDate() - i)
       end.setDate(end.getDate() - i)
       this._getStepCount(start, end)
-      //console.log("start : " + start + "     end : " + end)
+      this.setState({
+        isLoading: false
+      })
+      /*console.log("start : ")
+      console.log(start)
+      console.log("end : ")
+      console.log(end)*/
     }
   };
 
-  async _getStepCount(start, end) {
+  _getStepCount(start, end) {
     var day = days[start.getDay()]
-    await Pedometer.getStepCountAsync(start, end).then(
+    Pedometer.getStepCountAsync(start, end).then(
       result => {
         this.setState({ pastStepCount: { ...this.state.pastStepCount, [day]: result.steps} });
         //console.log(result.steps)
       },
       error => {
-        this.setState({ pastStepCount: { ...this.state.pastStepCount, [day]: "Could not get stepCount: " + error} });
+        this.setState({ pastStepCount: { ...this.state.pastStepCount, [day]: "error: " + error} });
         //console.log("Could not get stepCount: " + error)
       }
     );
@@ -78,6 +86,16 @@ export default class DayPodometer extends React.Component {
     this._subscription && this._subscription.remove();
     this._subscription = null;
   };
+
+  _displayLoading() {
+    if(this.state.isLoading) {
+      return(
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large'/>
+        </View>
+      )
+    }
+  }
 
 /*<FlatList
   data={this.state.pastStepCount.monday}
@@ -95,6 +113,7 @@ export default class DayPodometer extends React.Component {
         <Text>Friday    {this.state.pastStepCount.friday}</Text>
         <Text>Saturday  {this.state.pastStepCount.saturday}</Text>
         <Text>Sunday    {this.state.pastStepCount.sunday}</Text>
+        {this._displayLoading()}
       </View>
     );
   }
@@ -106,6 +125,15 @@ const styles = StyleSheet.create({
     marginTop: 15,
     alignItems: "center",
     justifyContent: "center"
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
--- src/screens/Settings/DayStartStop.js
+++ src/screens/Settings/DayStartStop.js
@@ -1,34 +1,74 @@
 import React, { Component } from 'react';
-import { Text, TouchableOpacity, View, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {
+  Container,
+  Content,
+  Button,
+  Text,
+  Icon
+} from "native-base";
 import DateTimePicker from 'react-native-modal-datetime-picker';
 
 export default class DateTimePickerTester extends Component {
   state = {
-    isDateTimePickerVisible: false,
+    isTimePickerStartVisible: false,
+    isTimePickerEndVisible: false,
   };
 
-  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+  _showTimePickerStart = () => this.setState({ isTimePickerStartVisible: true });
+  _hideTimePickerStart = () => this.setState({ isTimePickerStartVisible: false });
+  _showTimePickerEnd = () => this.setState({ isTimePickerEndVisible: true });
+  _hideTimePickerEnd = () => this.setState({ isTimePickerEndVisible: false });
 
-  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+  _handlePickedStart = (date) => {
+    console.log('Start Time: ', date.getHours() + "h");
+    this._hideTimePickerStart();
+  };
 
-  _handleDatePicked = (date) => {
-    console.log('A date has been picked: ', date);
-    this._hideDateTimePicker();
+  _handlePickedEnd = (date) => {
+    console.log('End Time: ', date.getHours() + "h");
+    this._hideTimePickerEnd();
   };
 
   render () {
     return (
-      <View style={{ flex: 1 }}>
-        <Button title="Show timePicker" onPress={this._showDateTimePicker}/>
+      <Container style={styles.container}>
+        <Text>
+          Select time below for the start and end of your day for sending you messages. Messages will not be sent outside of these hours.
+        </Text>
+        <Button full onPress={this._showTimePickerStart}>
+          <Icon name="clock" />
+          <Text>Start of day</Text>
+        </Button>
         <DateTimePicker
-          isVisible={this.state.isDateTimePickerVisible}
-          onConfirm={this._handleDatePicked}
-          onCancel={this._hideDateTimePicker}
+          isVisible={this.state.isTimePickerStartVisible}
+          onConfirm={this._handlePickedStart}
+          onCancel={this._hideTimePickerStart}
           mode='time'
           is24Hour={false}
         />
-      </View>
+
+        <Button full onPress={this._showTimePickerEnd}>
+          <Icon name="clock" />
+          <Text>End of day</Text>
+        </Button>
+        <DateTimePicker
+          isVisible={this.state.isTimePickerEndVisible}
+          onConfirm={this._handlePickedEnd}
+          onCancel={this._hideTimePickerEnd}
+          mode='time'
+          is24Hour={false}
+        />
+      </Container>
     );
   }
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    paddingLeft: 30,
+    paddingRight: 30
+  }
+});
